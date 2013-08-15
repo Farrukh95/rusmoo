d421b8c324b1e2d2dbe011c71ceabba758173270
MytnikAA <>
2013-08-15 14:27:52 +0700

    enh: добавлент коментарий к документу (удалены вью кроме листа)

--- grails-app/domain/rusmoo/Document.groovy
+++ grails-app/domain/rusmoo/Document.groovy
@@ -4,11 +4,13 @@ class Document {
 
     String name
     byte[] data
+    String comment
     
     static constraints = {
-        name(nullable:true, size:0..150)
+        name(nullable:true, size:0..256)
+        comment(nullable:true, size:0..256)
         unit(nullable:true)
-        data(size: 0..10485760, render: false, blank: false) //10MB max size
+        data(size: 0..10485760, blank: false) //10MB max size
     }
     
     static belongsTo = [unit : Unit]
